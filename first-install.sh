3c9ddd2326b1e5abc8c910133b3d94b739c90665
layman <layman@localhost>
2019-07-24 13:18:23 +0100

    add libical ebuild

--- first-install.sh
+++ first-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir /etc/portage/package.mask/ /etc/portage/make.conf/;ln -sf /var/lib/layman/prototype99/profiles-local/custom /etc/portage/package.mask/custom;ln -sf /var/lib/layman/prototype99/profiles-local/dir /etc/portage/make.conf/dir;ln -sf /var/lib/layman/prototype99/profiles-local/lto /etc/portage/make.conf/lto
+mkdir /etc/portage/package.mask/;ln -sf /var/lib/layman/prototype99/profiles-local/custom /etc/portage/package.mask/custom;ln -sf /var/lib/layman/prototype99/profiles-local/make.conf /etc/portage/make.conf
