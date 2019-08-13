8f2ae42edae2cfcbb17380a775cba1f130e0d71a
layman <layman@localhost>
2019-08-13 17:11:28 +0100

    backup now

---
+++ first-install-new-dir.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mkdir /etc/portage/package.mask/;ln -sf /var/db/repos/prototype99/profiles-local/custom /etc/portage/package.mask/custom;ln -sf /var/db/repos/prototype99/profiles-local/make.conf /etc/portage/make.conf
