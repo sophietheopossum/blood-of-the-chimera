e29a9f479bb235f13ac349df9eff591102f560be
layman <layman@localhost>
2019-09-16 19:30:20 +0100

    sync overlay

--- first-install-new-dir.sh
+++ first-install-new-dir.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-mkdir /etc/portage/package.mask/;ln -sf /var/db/repos/prototype99/profiles-local/custom /etc/portage/package.mask/custom;ln -sf /var/db/repos/prototype99/profiles-local/make.conf /etc/portage/make.conf
+mkdir /etc/portage/package.mask/
+sudo ln -sf /var/db/repos/prototype99/profiles-local/custom /etc/portage/package.mask/custom
+sudo ln -sf /var/db/repos/prototype99/profiles-local/make.conf /etc/portage/make.conf
+ln -sf /var/db/repos/prototype99/profiles-local/kitty.conf ~/.config/kitty/kitty.conf
