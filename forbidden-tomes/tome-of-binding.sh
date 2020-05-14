2f9f6e84cd62cd7da4c8f5b618a7a8dd3afed41e
seirra <sophietheopossum@yandex.com>
2020-05-14 23:21:05 +0100

    add new rindeal hook, create scripts to maintain symlinks

---
+++ forbidden-tomes/tome-of-binding.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+X="sudo ln -f /var/db/repos/blood-of-the-chimera/forbidden-tomes/litany-of-claws/"
+Y=" /etc/paludis/hooks/sync_post/"
+Z1="dantrell-gnome-numbered.hook"
+Z2="dantrell-gnome.hook"
+Z3="net.hook"
+Z4="progress.hook"
+Z5="srcshelton.hook"
+Z6="rindeal.hook"
+${X}${Z1}${Y}${Z1}
+${X}${Z2}${Y}${Z2}
+${X}${Z3}${Y}${Z3}
+${X}${Z4}${Y}${Z4}
+${X}${Z5}${Y}${Z5}
+${X}${Z6}${Y}${Z6}
